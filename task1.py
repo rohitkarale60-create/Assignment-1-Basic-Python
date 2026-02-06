943ef66459728d201d9f75865306d3668b7b54b7
rohitkarale60-create <rohitkarale60@gmail.com>
2026-02-07 01:10:42 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,18 @@
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+
+
+if num2 != 0:
+    division = num1 / num2
+else:
+    division = "Error: Division by zero is not allowed"
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
